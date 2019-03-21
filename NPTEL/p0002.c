c6fa72de2d842713608eea3969b6c932d551616b
KodeSaga <kode.drama@gmail.com>
2019-03-21 22:16:49 +0530

    p0002

--- NPTEL/p0002.c
+++ NPTEL/p0002.c
@@ -1,4 +1,4 @@
-//[Complete]Problem statement - print prime numbers till a, assuming values greater than 3
+//[Complete]Problem statement - print prime numbers till a, assuming values greater than 3, assumption a>=5
 #include <stdio.h>
 #include <math.h>
 int main()
@@ -17,11 +17,9 @@ int main()
                         if ((j % i) == 0)
                                 flag = 1;
                         i += 2;
-                        //printf("here flag=%d j=%d i=%d\n",flag,j,i);
                 }
                 if (flag == 0)
                         printf("%d\n", j);
         }
-        //printf("%d\n",flag);
         return 0;
 }
