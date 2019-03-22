e625bcf7c4bfed0323d4d29193f57ef5813a2996
KodeSaga <kode.drama@gmail.com>
2019-03-22 14:55:39 +0530

    p0004 Problem statement

---
+++ NPTEL/p0004.c
@@ -0,0 +1,6 @@
+//[inComplete]Problem statement - print the patterns till n rows
+//  a)  *       b)      *       c)  ****     d) 1 2 3 4  e) 1       f)  5 6 7 8 9 8 7 6 5  g) Pascal 1     h) Floyd 1
+//      **             ***          ***         1 2 3       1 2           4 5 6 7 6 5 4             1 1             2 3
+//      ***           *****         **          1 2         1 2 3           3 4 5 4 3              1 2 1            4 5 6
+//      ****         *******        *           1           1 2 3 4           2 3 2               1 3 3 1           7 8 9 10
+//                                                                              1                1 4 6 4 1         11 12 13 14
