542ada8e10c0f01fae78213e35bbb9003d07849e
KodeSaga <kode.drama@gmail.com>
2019-03-22 12:05:24 +0530

    p0003 problem statement

---
+++ NPTEL/p0003.c
@@ -0,0 +1,6 @@
+//[inComplete]Problem statement - Base coversion from any base value to any other base 
+//For eg:- Octal to decimal , Decimal to Hexa, Binary to Octal.
+//Input -   B1 = initial Base
+//          N1 = initial Number
+//          B2 = Target Base
+//Output -  N2 = Target value in B2 base
--- readme.md
+++ readme.md
@@ -2,3 +2,4 @@
 |  :---:  |  :---: |  :---:  | :---:  |
 | p0001 | Complete | NPTEL | GCD or HCF of two numbers |
 | p0002 | Complete | NPTEL | Prime number Generation till a specific number |
+| p0003 | **InComplete** | NPTEL | Base coversion from any base value to any other base |
