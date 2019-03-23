ae7d7e15f9eb88b2208ff54b0b8470b7c003f5d9
KodeSaga <kode.drama@gmail.com>
2019-03-24 00:07:48 +0530

    p0010 completed

--- NPTEL/p0010.c
+++ NPTEL/p0010.c
@@ -1,7 +1,22 @@
 //[Complete]Problem statement - Write a C Program to find HCF of 4 given numbers using recursive function.
 #include <stdio.h>
 
+int hcf(int a, int b){
+        int temp;
+        if(b<a){
+                temp = a;
+                a = b;
+                b = temp;
+        }
+        if( b % a == 0 )
+                return a;
+        else
+                return hcf(a, b%a);
+}
+
 int main(){
-    
+        int a,b,c,d;
+        scanf("%d%d%d%d",&a,&b,&c,&d);
+        printf("The HCF is %d",hcf(a,hcf(b,hcf(c,d))));
         return 0;
 }
